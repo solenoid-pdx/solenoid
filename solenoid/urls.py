339448329862b6c4b72d025ca8bc6070b38e4246
Joseph Jesse <jjesse@pdx.edu>
2020-12-03 13:19:43 -0800

    Removing admin page

--- solenoid/urls.py
+++ solenoid/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from solenoid_app import views
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index)
+    # path('admin/', admin.site.urls),
+    path('', views.index),
 ]
