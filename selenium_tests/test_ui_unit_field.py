7bc1524479c89fe723d03aa37c9f0e024fbae2a4
Xiaomeng Jiang <jiang6@pdx.edu>
2021-02-11 10:41:06 -0800

    add testing the force unit

--- selenium_tests/test_ui_unit_field.py
+++ selenium_tests/test_ui_unit_field.py
@@ -11,7 +11,7 @@ from selenium_tests.selenium_test_base import SeleniumTestBase
 
 URL = "http://localhost:8000/"
 
-@tag('unit')
+@tag('ui')
 class TestUnitSelectTag(SeleniumTestBase):
     """ Test unit select field"""
 
@@ -19,7 +19,7 @@ class TestUnitSelectTag(SeleniumTestBase):
         super().__init__(methods)
 
     def test_select_tag_is_created(self):
-        """ Test every unit's select field and options are exist, also the selected value changed correctly """
+        """ Test every unit's select field and options are exist, and the selected value changed correctly """
 
         units = ['mm', 'cm', 'm', 'inch', 'feet']
         parameters = ['length','r_not','r_a','x']
@@ -29,3 +29,8 @@ class TestUnitSelectTag(SeleniumTestBase):
                 self.driver.find_element_by_xpath(".//*[@id='input-unit-" + parameter + "']/option[" + str(index) + "]").click()
                 index += 1
                 self.assertEqual(self.driver.find_element_by_id("input-unit-" + parameter).get_attribute('value'),unit)
+
+        self.driver.find_element_by_xpath(".//*[@id='input-unit-force']/option[1]").click()
+        self.assertEqual(self.driver.find_element_by_id("input-unit-force").get_attribute('value'), 'N')
+        self.driver.find_element_by_xpath(".//*[@id='input-unit-force']/option[2]").click()
+        self.assertEqual(self.driver.find_element_by_id("input-unit-force").get_attribute('value'), 'lbf')
